ae1591a6d6ca064d618380b5ac5e5668bd25e54d
Bycya <lyubov.g.2018@mail.ru>
2023-11-15 19:46:42 +0300

    Симулятор лифта

---
+++ WpfApp2/WpfApp2/Elevator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Elevator
+    {
+    public enum States
+    {
+        EmptyWaiting,
+        EmptyClimbing,
+        OpenDoorsWaiting
+    }
+
+    public event EventHandler<States> StateChanged;
+    public event EventHandler<int> LevelChanged;
+    public int Level { get; private set; }
+    public int Level_nyg { get; private set; }
+    public States State { get; private set; }
+
+    public Elevator()
+    {
+        Level = 1;
+        Level_nyg = 2;
+    }
+
+    public async Task GoTo(int level, int Level_nyg)
+    {
+        State = States.EmptyClimbing;
+        StateChanged?.Invoke(this, this.State);
+            if (level > Level_nyg)
+            {
+                for (int i = level; i >= Level_nyg; i--)
+                {
+                    await Task.Delay(1000);
+                    Level = i;
+                    LevelChanged?.Invoke(this, this.Level);
+                }
+            }
+            else if (Level_nyg > level)
+            {
+                for (int i = level; i <= Level_nyg; i++)
+                {
+                    await Task.Delay(1000);
+                    Level = i;
+                    LevelChanged?.Invoke(this, this.Level);
+                }
+            }
+            else if (Level_nyg == level)
+            {
+                LevelChanged?.Invoke(this, this.Level);
+            }
+
+        State = States.OpenDoorsWaiting;
+        StateChanged?.Invoke(this, this.State);
+    }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button button1 = new Button();
+            this.tb.PreviewTextInput += new TextCompositionEventHandler(tb_textInput);
+            Im1.Source = new BitmapImage(new Uri("/Resourse/1.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
+        }
+       
+        Elevator elevator;
+        async void FirstMetod(object sender, RoutedEventArgs e)
+        {
+            Im1.Source = new BitmapImage(new Uri("/Resourse/2.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
+            await Task.Delay(500);
+            elevator = new Elevator();
+            elevator.StateChanged += Elevator_StateChanged;
+            elevator.LevelChanged += Elevator_LevelChanged;
+            int level2 = int.Parse((sender as Button).Content.ToString());
+            int level = int.Parse(etaz.Content.ToString());
+            await elevator.GoTo(level, level2);
+
+        }
+        private void Elevator_LevelChanged(object sender, int e)
+        {
+            etaz.Content = elevator.Level.ToString();
+        }
+
+        private void Elevator_StateChanged(object sender, Elevator.States e)
+        {
+            switch (e)
+            {
+                case Elevator.States.EmptyClimbing:
+                    Im1.Source = new BitmapImage(new Uri("/Resourse/2.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
+                    break;
+                case Elevator.States.OpenDoorsWaiting:
+                    Im1.Source = new BitmapImage(new Uri("/Resourse/1.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache };
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void tb_textInput(object sender, TextCompositionEventArgs e) //для ввода только цифр
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+        }
+
+        private void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tb.Text != "")
+            {
+                if (int.Parse(tb.Text) > 25 || int.Parse(tb.Text) == 0)
+                {
+                    MessageBox.Show("Ограничение до 25 этажей");
+                    tb.Text = "";
+                }
+                else
+                {
+                    for(int i = 1; i < int.Parse(tb.Text)+1; i++)
+                    {
+                        Button button = new Button();
+                        button.Content = i.ToString();
+                        button.Click += FirstMetod;
+                        ButtonsPanel.Children.Add(button);
+                    }
+                }
+            }
+            else
+            {
+                while (ButtonsPanel.Children.Count > 0)
+                    ButtonsPanel.Children.RemoveAt(0);
+            }
+        }
+
+    }
+}
